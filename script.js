23fb0c0a330a854ff4b007d3b05a313189a24f9e
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-16 16:53:11 +0100

    adapt script.js to UI

--- script.js
+++ script.js
@@ -22,60 +22,75 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  return prompt("Rock, Paper, Scissors?");
-}
-
 function playGame() {
   let computerScore = 0;
   let humanScore = 0;
-  let round = 1;
 
   rockButton.addEventListener("click", () => {
     playRound("rock", getComputerChoice());
-  })
+  });
 
   paperButton.addEventListener("click", () => {
     playRound("paper", getComputerChoice());
-  })
+  });
 
   scissorsButton.addEventListener("click", () => {
     playRound("scissors", getComputerChoice());
-  })
+  });
 
   function playRound(humanChoice, computerChoice) {
     computerChoice = getComputerChoice();
+    let resultsText = "";
 
     if (humanChoice === computerChoice) {
-      console.log("It's a tie!");
+      resultsText = "It's a tie!";
     } else if (humanChoice === "rock") {
       if (computerChoice === "scissors") {
-        console.log("You win! Rock beats scissors.");
+        resultsText = "You win! Rock beats scissors.";
         humanScore++;
       } else {
-        console.log("You lose! Paper beats rock.");
+        resultsText = "You lose! Paper beats rock.";
         computerScore++;
       }
     } else if (humanChoice === "paper") {
       if (computerChoice === "rock") {
-        console.log("You win! Paper beats rock.");
+        resultsText = "You win! Paper beats rock.";
         humanScore++;
       } else {
-        console.log("You lose! Scissors beat paper.");
+        resultsText = "You lose! Scissors beat paper.";
         computerScore++;
       }
     } else if (humanChoice === "scissors") {
       if (computerChoice === "paper") {
-        console.log("You win! Scissors beat paper.");
+        resultsText = "You win! Scissors beat paper.";
         humanScore++;
       } else {
-        console.log("You lose! Rock beats scissors.");
+        resultsText = "You lose! Rock beats scissors.";
         computerScore++;
       }
     }
+    resultsDiv.innerHTML = `
+     <p>Result:</p>
+     <p>${resultsText}</p>
+     <p>Player score: ${humanScore}</p>
+     <p>Computer score: ${computerScore}</p>
+    `;
 
-    console.log("Player score: ", humanScore);
-    console.log("Computer score", computerScore);
+    if (humanScore === 5) { 
+      resultsDiv.innerHTML += `
+
+        You win!
+      `;
+      humanScore = 0;
+      computerScore = 0;
+    } else if (computerScore === 5) {
+      resultsDiv.innerHTML += `
+
+        You lose...
+      `;
+      humanScore = 0;
+      computerScore = 0;
+    }
   }
 }
 
